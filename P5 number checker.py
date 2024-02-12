ac4a521476269376c4c628aa6e1fd508784ff51e
Manling <110644990+MarlingHeli@users.noreply.github.com>
2024-02-13 10:03:05 +1300

    Add files via upload

---
+++ P5 number checker.py
@@ -0,0 +1,26 @@
+# ask user for width and loop until they enter a number > 0
+
+def num_check(question):
+    error = "Please enter a number more than zero\n"
+    while True:
+        try:
+            response = float(input(question))
+
+            if response > 0:
+                return response #ends loop
+            else:
+                print(error)
+
+        except ValueError:  # prevents string input
+            print(error)
+
+
+# Main routine here
+for item in range(0, 2):
+    width = num_check("Width: ")
+    print(width)
+
+for item in range(0, 2):
+    height = num_check("Height: ")
+    print(height)
+
